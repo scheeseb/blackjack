eb94ab3d5c3d7464fc46e7c6cf98ae0cec54e251
Jeff <jlc68@me.com>
2024-11-23 12:48:23 -0600

    added code to the script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,297 @@
-function cardBody(number, face) {
-    const cardBody = document.createElement("div");
-    cardBody.className = "playingCard"
-    cardBody.style.backgroundColor = "hsl(48, 39%, 83%)";
-    cardBody.style.display = "flex";
-    cardBody.style.width = "2.5in";
-    cardBody.style.height = "3.5in";
-    cardBody.style.borderRadius = "10px";
-    cardBody.style.position = "relative";
-    cardBody.style.padding = "15px"
-
-    function appendSpans() {
-        const color = ((face === "heart" || face === "diamond") && "red") || "black";
-        const span0 = document.createElement("span");
-        span0.style.position = "absolute";
-        span0.style.fontSize = "32px";
-        span0.style.fontWeight = "bolder";
-        span0.style.fontFamily = "serif";
-        span0.style.left = "10px";
-        span0.style.top = "10px"
-        span0.style.color = color;
-        span0.textContent = number
-        const span1 = document.createElement("span");
-        span1.style.position = "absolute";
-        span1.style.fontSize = "32px";
-        span1.style.fontWeight = "bolder";
-        span1.style.fontFamily = "serif";
-        span1.style.right = "10px";
-        span1.style.bottom = "10px"
-        span1.style.transform = "rotate(180deg)";
-        span1.style.color = color;
-        span1.textContent = number
-
-        cardBody.append(span0, span1)
-    }
-    appendSpans();
-    function columnns() {
-        const columnns = []
-        for (let i = 0; i < 3; i++) {
-            const columnn = document.createElement("div");
-            columnn.style.display = "flex";
-            columnn.style.flexDirection = "column";
-            columnn.style.flex = "1";
-            columnn.style.height = "100%"
-            columnns.push(columnn)
-
+class Deck {
+    constructor(howManyDecks = 1) {
+        this.deck = [];
+        for (let i = 0; i < howManyDecks; i++) {
+            for (let i = 0; i < 52; i++) {
+                this.deck.push(i)
+            }
         }
-        return columnns
+        this.shuffle()
     }
 
-    const columnnArray = columnns();
-    const leftColumnn = columnnArray[0]
-    const centerColumnn = columnnArray[1]
-    const rightColumnn = columnnArray[2]
-    const allSymbols = [];
+    // This function creates an array to index the cards for the game.  It first creates the 52 indexes sequentially, after doing so it shuffles them into a random order returning the randomized array.
+    shuffle() {
+        let array = this.deck
+        let tmp, current, top = this.deck.length;
 
-    if (typeof number === "number") {
-        const symbolSource = (face === "heart" && "./assets/cardAssets/symbols/hearts.png") ||
-            (face === "club" && "./assets/cardAssets/symbols/clubs.png") ||
-            (face === "diamond" && "./assets/cardAssets/symbols/diamonds.png") ||
-            (face === "spades" && "./assets/cardAssets/symbols/spades.png");
-        for (let i = 0; i < number; i++) {
-            const symbolElement = document.createElement("img");
-
-            symbolElement.src = symbolSource;
-            symbolElement.style.height = "auto";
-            symbolElement.style.width = "50%";
-            symbolElement.style.alignitems = "space-between"
-            symbolElement.style.margin = "auto"
-
-            allSymbols.push(symbolElement)
+        if (top) while (--top) {
+            current = Math.floor(Math.random() * (top + 1));
+            tmp = array[current];
+            array[current] = array[top];
+            array[top] = tmp;
         }
+        return array;
     }
-    cardBody.append(leftColumnn, centerColumnn, rightColumnn)
-
-    if (allSymbols.length < 4) {
-        allSymbols.forEach(Element => {
-            centerColumnn.append(Element)
-        });
-    }
-    let originalLength = allSymbols.length;;
-    if (allSymbols.length >= 4 && allSymbols.length <= 10) {
-        if (originalLength % 2 === 1) {
-            centerColumnn.append(allSymbols.pop());
-        }
-        originalLength = allSymbols.length;
-        for (let i = 0; i < originalLength / 2; i++) {
-            leftColumnn.append(allSymbols.pop())
-            rightColumnn.append(allSymbols.pop())
-        }
-    }
-
-    return cardBody
 }
-document.body.append(cardBody(8, "heart"))
+
+function extractCard(item){
+//13 is the number of cards per suite.  This logic extacts the value and suite of the card which in the deck is represented 0-51 for the 52 cards
+const cardInfo = {value: item %13, suite: Math.floor(item/13)}  
+return cardInfo;
+}
+
+let deck = new Deck;
+let card;
+
+let times = 58;
+while(times--){
+    if(deck.deck.length==0) {
+        deck = new Deck;
+        console.log("new deal");
+    }
+    card = extractCard(deck.deck.pop());
+    console.log(card);
+}
+
+// Accepts a 
+// *Card number 0-12
+// Suit = heart, spade, diamond, club
+
+/*
+pseudo code/game rules for game logic:
+    rules: all cards to player are dealt face up
+        value of aces are 1 or 11, rule to 1 or 11 if hand + 11 <= 21 then the ace is worth 11 otherwise its worth 1
+
+    
+    deal a card to player
+    deal a card to the dealer face down
+    deal a card to player
+    deal a card to the dealer face up
+
+    rules for blackjack after the deal
+        if player has blackjack flip dealers down card face up, check for dealer blackjack if dealer has blackjack its a push otherwise player wins
+        if the dealer has an ace for his up card (get the sum of the dealers hand) if the dealer has blackjack flip the down card to show the  blackjack, player loses
+
+    continuing game play past the initial deal
+        Player is asked to take a hit or stay
+            hit the player gets a card the value of the card is added to the hand if it exceeds 21 the player loses the hand
+            the player can hit as many times as they like until they stay or until they bust.
+        player stays play is turned over to dealer
+            if the dealer has a score of less than 17 they will take hits until their score >= 17 or they bust
+        after the dealer is done hitting or has stood the two hands are compared for scoring 
+        player > dealer player wins
+        player < dealer player loses
+        player = dealer its a push
+
+*/
+
+
+class Board {
+    constructor() {
+        this.dealerBoard = [];
+        this.playerBoard = [];
+    }
+    dealToDealer(cardObject) {
+        this.dealerBoard.push(cardObject)
+    }
+    dealToPlayer(cardObject) {
+        this.playerBoard.push(cardObject)
+    }
+    // TODO: Create a function that accepts an array of card objects and returns the sum
+    // TODO: Create a function that accepts a board array and returns "under", "blackjack", or "bust"
+}
+
+class Game {
+    constructor() {
+        this.storageKey = "gameSave";
+        this.board = this.loadGame()[0];
+        this.deck = this.loadGame()[1]
+    }
+    saveGame() {
+        localStorage.setItem("board", JSON.stringify(this.board));
+        localStorage.setItem("deck", JSON.stringify(this.deck))
+    }
+    loadGame() {
+        const savedBoardJson = localStorage.getItem("board");
+        const savedDeckJson = localStorage.getItem("deck");
+
+        let board = new Board;
+        let deck = new Deck(1)
+
+        if (savedBoardJson) {
+            board = JSON.parse(localStorage.getItem("board"));
+        } else {
+            localStorage.setItem("board", JSON.stringify(board));
+        }
+
+        if (savedDeckJson) {
+            deck = JSON.parse(localStorage.getItem("deck"));
+        } else {
+            localStorage.setItem("deck", JSON.stringify(deck))
+        }
+
+
+        return [board, deck]
+    }
+    // TODO: Create a funtion that clear the current boards
+    clearBoard() { }
+    // TODO: Create a function that replaces the current deck
+    newDeck() { }
+    // TODO: Create a function that returns the winner ('dealer' or 'player') or false if no one has won
+    winner() { }
+
+}
+
+// We can load a new game after every round. This will allow us to stash the old boards and keep a history
+const theGame = new Game
+console.log(theGame)
+
+
+class Ui {
+    constructor() { }
+    createCard(number, suit) {
+        // add one to 0 indexed input number
+        const inputNumber = number + 1;
+        // create a div for the card body
+        const cardBody = document.createElement("div");
+        cardBody.className = "playingCard"
+        cardBody.style.backgroundColor = "hsl(48, 39%, 83%)";
+        cardBody.style.display = "flex";
+        cardBody.style.width = "125px";
+        cardBody.style.height = "180px";
+        cardBody.style.borderRadius = "10px";
+        cardBody.style.position = "relative";
+        cardBody.style.padding = "15px";
+
+
+        // Creates two spans that are positioned absolutely
+        function appendSpans() {
+            function createSpan() {
+                const span = document.createElement("span");
+                span.style.position = "absolute";
+                span.style.fontSize = "32px";
+                span.style.fontWeight = "bolder";
+                span.style.fontFamily = "serif";
+                span.style.color = color;
+                span.textContent = inputNumber;
+
+                return span
+            }
+
+            const color = ((suit === "heart" || suit === "diamond") && "red") || "black";
+            const span0 = createSpan();
+            const span1 = createSpan()
+
+            // Position each span individually and rotate the second
+            span0.style.left = "10px";
+            span0.style.top = "10px";
+
+            span1.style.right = "10px";
+            span1.style.bottom = "10px"
+            span1.style.transform = "rotate(180deg)";
+
+
+            if (inputNumber === 11) {
+                span0.textContent = "J";
+                span1.textContent = "J";
+            }
+            if (inputNumber === 12) {
+                span0.textContent = "Q";
+                span1.textContent = "Q";
+            }
+            if (inputNumber === 13) {
+                span0.textContent = "K";
+                span1.textContent = "K";
+            }
+
+            cardBody.append(span0, span1);
+        }
+        appendSpans();
+
+        // Creates 3 equal sized coloums and returns them in an array
+        function columns() {
+            const columns = []
+            for (let i = 0; i < 3; i++) {
+                const column = document.createElement("div");
+                column.style.display = "flex";
+                column.style.flexDirection = "column";
+                column.style.flex = "1";
+                column.style.height = "100%";
+                columns.push(column);
+
+            }
+            return columns;
+        }
+
+        // create an array and assign each column as the appropriate variable
+        const columnArray = columns();
+        const leftColumn = columnArray[0];
+        const centerColumn = columnArray[1];
+        const rightColumn = columnArray[2];
+        const allSymbols = [];
+
+        if (typeof inputNumber === "number") {
+            // Determine which symbol to load depending on the suit
+            const symbolSource = (suit === "heart" && "./assets/cardAssets/symbols/hearts.png") ||
+                (suit === "club" && "./assets/cardAssets/symbols/clubs.png") ||
+                (suit === "diamond" && "./assets/cardAssets/symbols/diamonds.png") ||
+                (suit === "spade" && "./assets/cardAssets/symbols/spades.png");
+
+            // Create as many symbols as the input number and add them to the allSymbols array 
+            for (let i = 0; i < inputNumber; i++) {
+                const symbolElement = document.createElement("img");
+
+                symbolElement.src = symbolSource;
+                symbolElement.style.height = "auto";
+                symbolElement.style.width = "50%";
+                symbolElement.style.alignitems = "space-between"
+                symbolElement.style.margin = "auto"
+
+                allSymbols.push(symbolElement)
+            }
+        }
+        cardBody.append(leftColumn, centerColumn, rightColumn)
+
+        // IF the symbol array is 3 or less, add all of the symbols to the center div
+        if (allSymbols.length < 4) {
+            allSymbols.forEach(Element => {
+                centerColumn.append(Element)
+            });
+        }
+        // Define the original length for use in the calculations
+        let originalLength = allSymbols.length;;
+
+        // IF there are between 4 and 10 symbols
+        if (allSymbols.length >= 4 && allSymbols.length <= 10) {
+            // IF there are an odd numnber of symbols add one to the center column
+            if (originalLength % 2 === 1) {
+                centerColumn.append(allSymbols.pop());
+            }
+            originalLength = allSymbols.length;
+
+            // Add a symbol to each outside column alernating to maintain the balance of the colums
+            for (let i = 0; i < originalLength / 2; i++) {
+                leftColumn.append(allSymbols.pop())
+                rightColumn.append(allSymbols.pop())
+            }
+        }
+        // IF it is a jack, apply the jack background image and paste in two symbols
+        if (allSymbols.length === 11) {
+            cardBody.style.backgroundImage = ((suit === "heart" || suit === "diamond") && 'url("./assets/cardAssets/svg/jackRed.svg")') || 'url("./assets/cardAssets/svg/jackBlack.svg")'
+            cardBody.style.backgroundRepeat = "no-repeat";
+            cardBody.style.backgroundPosition = "center";
+            centerColumn.append(allSymbols.pop(), allSymbols.pop())
+
+        } else if (allSymbols.length === 12) {
+            // IF it is a queen, apply the queen background image and paste in a symbol
+            cardBody.style.backgroundImage = ((suit === "heart" || suit === "diamond") && 'url("./assets/cardAssets/svg/queenRed.svg")') || 'url("./assets/cardAssets/svg/queenBlack.svg")'
+            cardBody.style.backgroundRepeat = "no-repeat";
+            cardBody.style.backgroundPosition = "center";
+            cardBody.style.backgroundSize = "contain"
+            centerColumn.append(allSymbols.pop())
+        } else if (allSymbols.length === 13) {
+            // IF it is a king, apply the king background image and paste in a symbol
+            cardBody.style.backgroundImage = ((suit === "heart" || suit === "diamond") && 'url("./assets/cardAssets/svg/kingRed.svg")') || 'url("./assets/cardAssets/svg/kingBlack.svg")'
+            cardBody.style.backgroundRepeat = "no-repeat";
+            cardBody.style.backgroundPosition = "center";
+            cardBody.style.backgroundSize = "contain";
+            centerColumn.append(allSymbols.pop());
+        }
+        return cardBody
+    }
+    // TODO: Make the buttons work with game functionallity
+}
+
+function init() {
+    // TODO: Make the Game and the UI interact here. Buttons and such
+}
